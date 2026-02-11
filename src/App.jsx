799a60643ee98b26b96872b4ea8e7877bcd230bb
deepakgudi-pixel <japanesen52018@gmail.com>
2026-02-11 15:23:07 +0400

    feature/windows draggable

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
-import { Navbar, Dock } from "#components";
 import gsap from "gsap";
 import { Draggable } from "gsap/Draggable";
 
+import { Navbar, Dock } from "#components";
+import { Terminal } from "#windows";
+
+
 
 gsap.registerPlugin(Draggable);
+
 const App = () => {
   return (
     <main>
       <Navbar />
       <Dock />
+
+      <Terminal />
     </main>
   );
 };
