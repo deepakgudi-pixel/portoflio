5e9624c76103f01284d5612ede8088ba5f24738d
deepakgudi-pixel <japanesen52018@gmail.com>
2026-02-12 13:09:09 +0400

    feature/windows terminal

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,5 @@
 import Navbar from "#components/Navbar";
 import Dock from "#components/Dock";
+import WindowControls from "#components/WindowControls";
 
-
-export { Navbar, Dock}
+export { Navbar, Dock, WindowControls };
